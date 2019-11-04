a3cc6f466014427d46c12eb4f60c49dd016c9e34
Igor Pergenitsa <ipergenitsa@gmail.com>
2019-11-04 16:35:30 +0600

    Add a paragraph describing implementation Rename main function Add a class representing a chain

--- canvas.js
+++ canvas.js
@@ -2,27 +2,12 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 let barWidth = 30;
-let chainLinkLength = 10;
 
 function drawTopBar(width) {
     let oldColor = ctx.fillStyle;
-    ctx.fillStyle = "#543623";
+    ctx.fillStyle = "maroon";
     ctx.fillRect(0, 0, canvas.width, width);
     ctx.fillStyle = oldColor;
-}
-
-function drawChain(chain) {
-    ctx.moveTo(canvas.width / 2.0, barWidth);
-    chain.forEach(chainLink => {
-        ctx.lineTo(chainLink.x, chainLink.y + barWidth);
-    });
-    ctx.stroke();
-
-    ctx.beginPath();
-    let lastChainLink = chain.slice(-1)[0];
-    ctx.arc(lastChainLink.x, lastChainLink.y + barWidth, lastChainLink.length, 0, 2 * Math.PI, true);
-    ctx.lineWidth = 0;
-    ctx.fill();
 }
 
 class ChainLink {
@@ -33,28 +18,57 @@ class ChainLink {
     }
 }
 
-let chain = []
-for (var i = 0; i < 40 ; i++) {
-    chain.push(new ChainLink(canvas.width / 2.0, i * chainLinkLength, chainLinkLength))
+class Chain {
+    constructor(firstLinkX, firstLinkY, linksNumber, linkLength) {
+        this.x = firstLinkY;
+        this.y = firstLinkY;
+        this.linkLength = linkLength;
+
+        this.links = [];
+        for (var i = 0; i < linksNumber ; i++) {
+            let link = new ChainLink(firstLinkX, i * linkLength + firstLinkY, linkLength);
+            this.links.push(link);
+        }
+    }
+
+    move() {
+        for (let i = 0; i < this.links.length - 1; i++) {
+            let currentLink = this.links[i];
+            let nextLink = this.links[i + 1];
+    
+            let dx = Math.sin(Date.now() / 300) * (this.linkLength - 1);
+            nextLink.x = currentLink.x + dx;
+            let dy = Math.sqrt(Math.abs(dx * dx - nextLink.length * nextLink.length));
+            nextLink.y = currentLink.y + dy;
+        }
+    }
+
+    draw(canvasCtx) {
+        canvasCtx.moveTo(this.x, this.y);
+        canvasCtx.beginPath();
+        this.links.forEach(chainLink => {
+            canvasCtx.lineTo(chainLink.x, chainLink.y);
+        });
+        canvasCtx.stroke();
+
+        canvasCtx.beginPath();
+        let lastChainLink = this.links.slice(-1)[0];
+        canvasCtx.arc(lastChainLink.x, lastChainLink.y, lastChainLink.length, 0, 2 * Math.PI, true);
+        canvasCtx.fill();
+        canvasCtx.stroke();
+    }
 }
 
-function drawer() {
+let chain = new Chain(canvas.width / 2, barWidth, linksNumber = 40, linkLength = 10);
+
+function main() {
     ctx.beginPath();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.stroke();
 
-    for (let i = 0; i < chain.length - 1; i++) {
-        let currentLink = chain[i];
-        let nextLink = chain[i + 1];
+    chain.move();
 
-        let dx = Math.sin(Date.now() / 300) * (chainLinkLength - 1);
-        nextLink.x = currentLink.x + dx;
-        let dy = Math.sqrt(Math.abs(dx * dx - nextLink.length * nextLink.length));
-        nextLink.y = currentLink.y + dy;
-    }
     drawTopBar(barWidth);
-    drawChain(chain);
+    chain.draw(ctx);
 }
 
-drawer();
-let mainInterval = setInterval(drawer, 10);
+let mainInterval = setInterval(main, 10);
